ea129b803975c3522e0607514b5eb36c192882d8
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2024-10-16 21:57:02 +0900

    Add counterProvider

--- lib/sandbox/CounterWithRiverpod/Counter.dart
+++ lib/sandbox/CounterWithRiverpod/Counter.dart
@@ -1,5 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+final counterProvider = StateNotifierProvider<Counter, int>((_) => Counter());
+
 class Counter extends StateNotifier<int> {
   Counter() : super(0);
 
