df5b39f55e716a21074345ff76d599688260101e
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2024-11-02 16:07:02 +0900

    Add ThreeDimensionalButton

---
+++ lib/widgets/three_dimensional_button.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ThreeDimensionalButton extends StatefulWidget {
+  final VoidCallback? onPressed;
+  final Widget? child;
+  final Color backgroundColor;
+  final Color shadowColor;
+
+  const ThreeDimensionalButton({
+    super.key,
+    required this.onPressed,
+    required this.child,
+    this.backgroundColor = Colors.white,
+    this.shadowColor = Colors.black,
+  });
+
+  @override
+  State<ThreeDimensionalButton> createState() => _ThreeDimensionalButtonState();
+}
+
+class _ThreeDimensionalButtonState extends State<ThreeDimensionalButton> {
+  bool isHighlighted = false;
+
+  double get topPadding => isHighlighted ? 2 : 0;
+  double get bottomPadding => isHighlighted ? 0 : 2;
+  double get shadowOffsetY => isHighlighted ? 0 : 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) {
+        HapticFeedback.mediumImpact();
+        setState(() {
+          isHighlighted = true;
+        });
+      },
+      onTapCancel: () {
+        setState(() {
+          isHighlighted = false;
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          isHighlighted = false;
+        });
+      },
+      onTap: () {
+        widget.onPressed?.call();
+      },
+      child: Padding(
+        padding: EdgeInsets.only(top: topPadding, bottom: bottomPadding),
+        child: Container(
+          decoration: BoxDecoration(
+              color: widget.backgroundColor,
+              border: Border.all(width: 2, color: widget.shadowColor),
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: widget.shadowColor,
+                  offset: Offset(0, shadowOffsetY),
+                  blurRadius: 0,
+                  spreadRadius: 0,
+                ),
+              ]),
+          child: widget.child,
+        ),
+      ),
+    );
+  }
+}
