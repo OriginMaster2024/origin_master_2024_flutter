bf766c89c773e844c974735dc172b2124ddb727c
Hatsune Mineta <hatsune.mineta@smartnews.com>
2024-11-02 14:23:02 +0900

    Add initial pages

---
+++ lib/page/hot_dog_select_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HotDogSelectPage extends StatelessWidget {
+  const HotDogSelectPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ホットドッグ'),
+      ),
+    );
+  }
+}
