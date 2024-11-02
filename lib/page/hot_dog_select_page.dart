446e0cadb3106c0c41aedb413f684c18b2284931
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2024-11-02 22:29:29 +0900

    Replace text with image

--- lib/page/hot_dog_select_page.dart
+++ lib/page/hot_dog_select_page.dart
@@ -103,10 +103,8 @@ class IngredientCard<T extends Ingredient> extends HookWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                ingredient.name,
-                style: AppTextStyle.bold(color: Colors.black, fontSize: 26),
-              ),
+              ingredient.titleImage.image(height: 26),
+              const Gap(8),
               Text(
                 ingredient.description,
                 style: AppTextStyle.medium(color: Colors.black, fontSize: 12),
