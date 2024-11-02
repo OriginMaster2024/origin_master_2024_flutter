7629be8693c08db87fd05e9a2e80a18b23a0effa
nukotsuka <nukotsuka@gmail.com>
2024-11-02 16:36:36 +0900

    Update HomePage

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -12,23 +12,123 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart' as _svg;
 import 'package:vector_graphics/vector_graphics.dart' as _vg;
 
+class $AssetsPngGen {
+  const $AssetsPngGen();
+
+  /// File path: assets/png/home_label.png
+  AssetGenImage get homeLabel =>
+      const AssetGenImage('assets/png/home_label.png');
+
+  /// File path: assets/png/home_top_bar.png
+  AssetGenImage get homeTopBar =>
+      const AssetGenImage('assets/png/home_top_bar.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [homeLabel, homeTopBar];
+}
+
 class $AssetsSvgGen {
   const $AssetsSvgGen();
 
   /// File path: assets/svg/bread.svg
   SvgGenImage get bread => const SvgGenImage('assets/svg/bread.svg');
 
+  /// File path: assets/svg/hotdog.svg
+  SvgGenImage get hotdog => const SvgGenImage('assets/svg/hotdog.svg');
+
   /// File path: assets/svg/sausage.svg
   SvgGenImage get sausage => const SvgGenImage('assets/svg/sausage.svg');
 
   /// List of all assets
-  List<SvgGenImage> get values => [bread, sausage];
+  List<SvgGenImage> get values => [bread, hotdog, sausage];
 }
 
 class Assets {
   Assets._();
 
+  static const $AssetsPngGen png = $AssetsPngGen();
   static const $AssetsSvgGen svg = $AssetsSvgGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
 }
 
 class SvgGenImage {
