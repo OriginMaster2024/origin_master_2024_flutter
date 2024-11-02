944bcb26427f7c6665c7039cf10154dd6f9b3734
Hatsune Mineta <hatsune.mineta@smartnews.com>
2024-11-02 17:09:28 +0900

    impl bread and sausage

--- lib/page/hot_dog_select_page.dart
+++ lib/page/hot_dog_select_page.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:origin_master_2024_flutter/gen/assets.gen.dart';
 import 'package:origin_master_2024_flutter/page/situation_page.dart';
 
 class HotDogSelectPage extends HookWidget {
@@ -22,12 +23,12 @@ class HotDogSelectPage extends HookWidget {
             const IngredientCard(type: 'ソーセージ', options: Sausage.values),
             TextButton(
               onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute<SituationPage>(
-                builder: (context) => const SituationPage(),
-              ),
-            );
-          },
+                Navigator.of(context).push(
+                  MaterialPageRoute<SituationPage>(
+                    builder: (context) => const SituationPage(),
+                  ),
+                );
+              },
               child: const Text('診断開始'),
             ),
           ],
@@ -66,10 +67,11 @@ class IngredientCard extends HookWidget {
           Text('$typeの種類'),
           Text(selected.value?.name ?? ''),
           Text(selected.value?.description ?? ''),
-          if (selected.value == null) ElevatedButton(
-            onPressed: startSelecting,
-            child: const Text('選択'),
-          ),
+          if (selected.value == null)
+            ElevatedButton(
+              onPressed: startSelecting,
+              child: const Text('選択'),
+            ),
         ],
       ),
     );
@@ -78,31 +80,76 @@ class IngredientCard extends HookWidget {
 
 abstract class Ingredient {
   String get name;
+
   String get description;
+
+  SvgGenImage get image;
 }
 
 enum Bread implements Ingredient {
-  whiteBread('White Bread', 'A soft and fluffy white bread, perfect for sandwiches.'),
-  wholeWheat('Whole Wheat', 'A hearty and healthy whole wheat bread.'),
-  sourdough('Sourdough', 'A tangy sourdough bread with a crispy crust.');
+  regular('レギュラーロール', '基本のホットドッグ用ロールで、柔らかくて少し甘みがあるのが特徴です。'),
+  potato('ポテトロール', 'ジャガイモを使って作られたロールで、柔らかくもちもちした食感があります。'),
+  brioche('ブリオッシュロール', 'フランスのブリオッシュ生地を使ったパンで、バターの風味が豊かでリッチな味わいです。'),
+  wholeWheat('全粒粉ロール', 'ヘルシー志向の方に人気で、全粒粉を使っているため香ばしく、栄養価も高いです。');
+
+  const Bread(this.name, this.description);
 
   @override
   final String name;
   @override
   final String description;
 
-  const Bread(this.name, this.description);
+  @override
+  SvgGenImage get image {
+    switch (this) {
+      case Bread.regular:
+        return Assets.svg.breadRegular;
+      case Bread.potato:
+        return Assets.svg.breadPotato;
+      case Bread.brioche:
+        return Assets.svg.breadBrioche;
+      case Bread.wholeWheat:
+        return Assets.svg.breadWholeWheat;
+    }
+  }
 }
 
 enum Sausage implements Ingredient {
-  porkSausage('Pork Sausage', 'A savory pork sausage with a juicy flavor.'),
-  beefSausage('Beef Sausage', 'A rich beef sausage with a bold taste.'),
-  chickenSausage('Chicken Sausage', 'A lighter sausage made from chicken.');
+  chorizo(
+      'チョリソー',
+      'ピリッとした辛さがあり、熟成されており、風味が濃厚です。ピリ辛の味わいがアクセントになります。',
+  ),
+  bratwurst(
+    'ブラートヴルスト',
+    'ドイツを代表するソーセージで、マイルドでジューシーな味わいが特徴ですが、ハーブやスパイスで軽く味付けされており、食べ応えがあります。',
+  ),
+  frankfurter(
+      'フランクフルト',
+      'ドイツのフランクフルト発祥のソーセージで、スモークされていることが多く、肉の旨味が強調されています。',
+  ),
+  wiener(
+    'ウィンナー',
+    'オーストリアのウィーン発祥のソーセージで、細くて短めのサイズが特徴。スモークしてあることが多く、パリッとした食感です。',
+  );
+
+  const Sausage(this.name, this.description);
 
   @override
   final String name;
   @override
   final String description;
 
-  const Sausage(this.name, this.description);
+  @override
+  SvgGenImage get image {
+    switch (this) {
+      case chorizo:
+        return Assets.svg.sausageChorizo;
+      case bratwurst:
+        return Assets.svg.sausageBratwurst;
+      case frankfurter:
+        return Assets.svg.sausageFrankfurter;
+      case wiener:
+        return Assets.svg.sausageWiener;
+    }
+  }
 }
